211431edd818300b85c7df73ebba55b8fa5b4fd9
Onur <114233145+Kheng34@users.noreply.github.com>
2022-10-27 20:20:53 +0300

    Log in system integrated

--- MainSystem/MainSystem.cpp
+++ MainSystem/MainSystem.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <windows.h>
 using namespace std;
 
-string cUsername = "onur";
-string cPassword = "sifre";
-string tUsername, tPassword;
-bool getIn = 0;
-int hallway;
+int hallway, hallway2, hallway3;
+int id;
+string name, pass;
+string nameNew, passNew;
+string newUserName, newUserPass;
+string databaseStr;
+int totalUsers = 0;
 
 int nuclearHallway;
 string nuclearCode = "0NURK1L1C";
@@ -16,27 +22,85 @@ int calculatorHallway;
 float num1, num2, result;
 char calculatorStartAgain = 'Y';
 
-void logInTest() {
-	while(getIn == 0){
+bool logInTest = 0;
+string passTry, nameTry;
+
+fstream database("database.txt");
+
+void databaseToStr() {
+	stringstream buffer;
+	database.open("database.txt");
+	buffer << database.rdbuf();
+	databaseStr = buffer.str();
+	cout << databaseStr << endl << endl; //DevMode
+	database.close();
+}
+
+void strToDatabase() {
+	database.open("database.txt",ios::out|ios::trunc);
+	database << databaseStr;
+	cout << databaseStr << endl << endl; //DevMode
+	database.close();
+}
+
+void findData() {
+	for(int i = 2*id-1; i > 0; i--) {
+		database.ignore(100,' ');	
+	}
+}
+
+void logIn() {
+	while(logInTest == 0) {
 		cout << "Username: ";
-		cin >> tUsername;
+		cin >> nameTry;
 		cout << "Password: ";
-		cin >> tPassword;
-		if(tUsername == cUsername) {
-			if(tPassword == cPassword) {
-				getIn = 1;
-				cout << "Login successful!" << endl;
+		cin >> passTry;
+		if(nameTry == name) {
+			if(passTry == pass) {
+				logInTest = 1;
+				cout << "You're in!" << endl;
 			}
 			else {
-				cout << "Login failed!" << endl;
-				cout << "Incorrect username or password entered!" << endl;
+				cout << "Wrong Username or Password!" << endl;
 			}
 		}
 		else {
-		cout << "Login failed!" << endl;
-		cout << "Incorrect username or password entered!" << endl;
+			cout << "Wrong Username! or Password!" << endl; //DevMode
 		}
 	}
+}
+
+void changeData() {
+	cout << "1 > Change Username\n2 > Change Pasword\n";
+	cin >> hallway2;	
+	if(hallway2 == 1) {
+		logIn();
+		cout << "New Username: ";
+		cin >> nameNew;
+		databaseToStr();
+		databaseStr.replace(databaseStr.find(name),name.length(),nameNew);
+		strToDatabase();
+	}
+	else if(hallway2 == 2) {
+		logIn();
+		cout << "New Password: ";
+		cin >> passNew;
+		databaseToStr();
+		databaseStr.replace(databaseStr.find(pass),pass.length(),passNew);
+		strToDatabase();
+	}
+}
+
+void countUsers() {
+	database.open("database.txt");
+	string line;
+	while(!database.eof()) {
+		getline(database, line);
+		totalUsers++;
+	}
+	line = "";
+	database.close();
+	cout << totalUsers << endl; //DevMode
 }
 
 void calculator() {
@@ -95,30 +159,51 @@ void nuclearSystem() {
 	}
 }
 
-void changeUsername() {
-	
-}
-
-void changePassword() {
-	
-}
-
 int main() {
-	cout << "Welcome!" << endl << "Please Log In" << endl;
-	logInTest();
-	cout << "1 > Calculator" << endl << "2 > !Nuclear System!" << endl << "3 > Change Username" << endl << "4 > Change Password" << endl;
+	database.close();
+	cout << "Welcome!" << endl << "1 > Log In\n2 > Sign Up\n";
 	cin >> hallway;
 	if(hallway == 1) {
-		calculator();
+		database.open("database.txt");
+		cout << "Id: ";
+		cin >> id;
+		findData();
+		database >> name;
+		database >> pass;
+		database.close();
+		cout << name << endl; //DevMode
+		cout << pass << endl; //DevMode
+		logIn();
+		cout << "1 > Calculator\n2 > !Nuclear System!\n3 > Account Settings\n";
+		cin >> hallway3;
+		if(hallway3 == 1) {
+			calculator();
+		}
+		else if(hallway3 == 2) {
+			nuclearSystem();
+		}
+		else if(hallway3 == 3) {
+			changeData();
+		}
 	}
 	else if(hallway == 2) {
-		nuclearSystem();
-	}
-	else if(hallway == 3) {
-		changeUsername();
-	}
-	else if(hallway == 4) {
-		changePassword();
-	}
-	return 0;
+		cout << "Your Username: ";
+		cin >> newUserName;
+		cout << "Your Password: ";
+		cin >> newUserPass;
+		countUsers();
+		databaseToStr();
+		database.open("database.txt", ios::out | ios::app);
+		database << endl << totalUsers + 1 << " " << newUserName << " " << newUserPass;
+		database.close();
+		databaseToStr();
+		HANDLE  hConsole;
+		hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(hConsole, 10);
+		cout << "Please note your id below, you need that for log in" << endl;
+		cout << "Your Id is = " << totalUsers + 1 << endl;
+		SetConsoleTextAttribute(hConsole, 7);
+	}	
+	//The End
+	cout << endl;system("PAUSE");return 0;
 }
